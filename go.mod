db4181e19dfe2bbc2d9a4e15b58d90cff55fa84b
tjhowse <tjhowse@gmail.com>
2024-07-21 22:52:17 +1000

    Module imports or something

--- go.mod
+++ go.mod
@@ -7,4 +7,12 @@ require (
 	golang.org/x/time v0.5.0
 )
 
-require github.com/caarlos0/env/v11 v11.1.0 // indirect
+require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/caarlos0/env/v11 v11.1.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/influxdata/influxdb-client-go/v2 v2.13.0 // indirect
+	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/oapi-codegen/runtime v1.0.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+)
---
+++ influxdb.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
+	"github.com/tjhowse/aus_grocery_price_database/internal/shared"
+)
+
+type influxDB struct {
+	db       influxdb2.Client
+	writeAPI api.WriteAPI
+}
+
+func (i *influxDB) Init(url, token, org, bucket string) {
+	i.db = influxdb2.NewClient(url, token)
+	i.writeAPI = i.db.WriteAPI(org, bucket)
+}
+
+func (i *influxDB) WriteDatapoint(info shared.ProductInfo) {
+	p := influxdb2.NewPoint("product",
+		map[string]string{"name": info.Name, "store": info.Store, "location": info.Location},
+		map[string]interface{}{"price": info.Price, "weight": info.WeightGrams},
+		info.Timestamp,
+	)
+	i.writeAPI.WritePoint(p)
+}
+
+// WriteWorker writes ProductInfo to InfluxDB
+// Note that the underlying library automatically batches writes
+// so we don't need to worry about that here.
+func (i *influxDB) WriteWorker(input <-chan shared.ProductInfo) {
+	for info := range input {
+		i.WriteDatapoint(info)
+	}
+}
+
+func (i *influxDB) Close() {
+	i.writeAPI.Flush()
+	i.db.Close()
+}
