a96dd72f1894d99ad428318cf2695c549f21cabc
tjhowse <tjhowse@gmail.com>
2024-07-24 17:14:02 +1000

    Test a github deployment

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	woolworths "github.com/tjhowse/aus_grocery_price_database/internal/woolworths"
 )
 
-const VERSION = "0.0.6"
+const VERSION = "0.0.7"
 
 type config struct {
 	InfluxDBURL                 string `env:"INFLUXDB_URL"`
