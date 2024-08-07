d8da25dbcf9e2fc333c5577db8049bb38c930834
tjhowse <tjhowse@gmail.com>
2024-08-07 10:08:30 +1000

    Add baby and health beauty

--- internal/woolworths/woolworths.go
+++ internal/woolworths/woolworths.go
@@ -90,8 +90,8 @@ func (w *Woolworths) Init(baseURL string, dbPath string, productMaxAge time.Dura
 		"1_5AF3A0A":  true, // Drinks
 		"1_8E4DA6F":  true, // Liquor
 		// "1_61D6FEB":  true, // Pet // Don't put this one in. It contains 10000 "pet" products that are all 1-each doses of pet medicine or something.
-		// "1_717A94B":  true, // Baby
-		// "1_894D0A8":  true, // Health & Beauty
+		"1_717A94B": true, // Baby
+		"1_894D0A8": true, // Health & Beauty
 		// "1_2432B58":  true, // Household
 		// "1_B63CF9E":  true, // Front of store
 
